b8dc6b3c3a5e10a5b0b3258fc58967ed563246fc
Samiullah <samiullah.khan.1989@gmail.com>
2017-04-27 15:56:03 +0500

    Done with the app by adding custom filters

--- app/app.modules.ts
+++ app/app.modules.ts
@@ -6,6 +6,8 @@ import { AppComponent } from "./component.app";
 import { ArtistItemComponent } from "./component.artist-item";
 import { ArtistDetailsComponent } from "./component.artist-details";
 
+import { SearchPipe } from "./pipe.search";
+
 @NgModule({
   imports: [ // imports features that we gonna need 
     BrowserModule,
@@ -13,7 +15,7 @@ import { ArtistDetailsComponent } from "./component.artist-details";
   ],
   declarations: [ //view classes that belongs to this module, it allows us to import all the modules that 
   // we have in our application
-    AppComponent, ArtistItemComponent, ArtistDetailsComponent
+    AppComponent, ArtistItemComponent, ArtistDetailsComponent, SearchPipe
   ],
   bootstrap: [ // main container of your entire application,root component
     AppComponent
