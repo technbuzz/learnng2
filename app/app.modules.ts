3bca1b4d22ee34cac01ce9adc3ff82bdd9267862
Samiullah <samiullah.khan.1989@gmail.com>
2017-04-27 11:16:49 +0500

    Worked on data binding

--- app/app.modules.ts
+++ app/app.modules.ts
@@ -1,11 +1,13 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./component.app";
 
 @NgModule({
   imports: [ // imports features that we gonna need 
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   declarations: [ //view classes that belongs to this module, it allows us to import all the modules that 
   // we have in our application
--- app/component.app.ts
+++ app/component.app.ts
@@ -7,5 +7,40 @@ import { Component } from "@angular/core";
 
 
 export class AppComponent {
-  name = 'Waleed Ahmad'
+  name : string;
+  artists : any; // we use any when we have complex data type
+
+
+  onClick(myItem, myElement){
+    this.name = myItem.name;
+    myElement.style.backgroundColor = '#fece4e';
+  }
+
+  addArtist(value){
+    if(value !== 0){
+      this.artists.push({
+        name: value,
+        school: 'Hard Knox'
+      })
+    }
+  }
+
+
+  constructor(){
+    this.name = "Samiullah Khan";
+    this.artists = [
+      {
+        name: "Justing Seeley",
+        school: "Penn State"
+      },
+      {
+        name: "James Williamson",
+        school: "Columbia International"
+      },
+      {
+        name: "Morton Ranhandrikson",
+        school: "Universting of Florida"
+      }
+    ]
+  }
 }
